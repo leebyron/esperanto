0aa9f2b9a9846b7aa57466646d712aa99b852efe
Rich Harris <richard.a.harris@gmail.com>
2014-10-27 11:44:01 -0400

    tidy up

--- src/Module/prototype/toAmd/index.js
+++ src/Module/prototype/toAmd/index.js
@@ -1,22 +1,15 @@
 import Source from '../../../Source';
 import getIntro from './getIntro';
-import getBody from './getBody';
 import transformSource from './transformSource';
 
 export default function Module$toAmd ( options ) {
-	var source,
-		intro,
-		body,
-		outro;
+	var source;
 
 	source = new Source( this.source );
-
-	//intro = getIntro( this, options );
-	//body = getBody( this, options );
 	transformSource( source, this, options );
+
 	source.prepend( getIntro( this, options ) );
 	source.append( '\n\n});' );
-	//outro = '});';
 
 	return source.toString();
 }
