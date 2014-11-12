709006ca2086dbc22e2c3501fc285def04d2162d
Rich Harris <richard.a.harris@gmail.com>
2014-11-11 23:15:50 -0500

    implement resolve without path (for browser support)

--- src/utils/resolve.js
+++ src/utils/resolve.js
@@ -1,12 +1,23 @@
-import path from 'path';
-
 export default function resolve ( importPath, importerPath ) {
-	var resolved;
+	var resolved, importerParts, importParts;
 
 	if ( importPath[0] !== '.' ) {
 		resolved = importPath;
 	} else {
-		resolved = path.join( path.dirname( importerPath ), importPath );
+		importerParts = importerPath.split( '/' );
+		importParts = importPath.split( '/' );
+
+		importerParts.pop(); // get dirname
+		while ( importParts[0] === '..' ) {
+			importParts.shift();
+			importerParts.pop();
+		}
+
+		while ( importParts[0] === '.' ) {
+			importParts.shift();
+		}
+
+		resolved = importerParts.concat( importParts ).join( '/' );
 	}
 
 	return resolved.replace( /\.js$/, '' );
